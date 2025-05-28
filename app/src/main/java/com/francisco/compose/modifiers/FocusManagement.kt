81edaac0296cbe192447c4c5330f5e009db7b432
Francisco Sales <salesfrancisco73@gmail.com>
2025-05-28 10:31:06 -0400

    focus management

--- app/src/main/java/com/francisco/compose/modifiers/FocusManagement.kt
+++ app/src/main/java/com/francisco/compose/modifiers/FocusManagement.kt
@@ -3,13 +3,10 @@ package com.francisco.compose.modifiers
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.focusGroup
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
